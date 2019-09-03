674e643bb209639f16611db1dff1425c969db91e
Jeeken Wang <i@wangziq.in>
2019-09-04 00:13:41 +0800

    implement linked_list in lab1

--- lab1/linked_list.c
+++ lab1/linked_list.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include "linked_list.h"
 
 node *linked_list_init(){
@@ -17,6 +18,7 @@ void linked_list_free(node *head){
     }
 }
 
+// FIXME: buffer overflow!
 char linked_list_string[0x10000];
 
 char *linked_list_tostring(node *head){
@@ -56,3 +58,112 @@ void linked_list_append(node *head, int val){
 
 /* your implementation goes here */
 
+/* insert val at position index */
+void linked_list_insert(node *head, int val, int index) {
+    // do nothing if index is out of bound
+    if (index > head->count || index < 0)
+        return;  // check if is tail?
+    // move to index
+    node *cur = head;
+    for (int i = 0; i < index; ++i) {
+        cur = cur->next;
+    }
+    // insert a new node
+    node *new_node = (node*)malloc(sizeof(node));
+    new_node->value = val;
+    new_node->next = cur->next;
+    cur->next = new_node;
+    head->count++;
+}
+
+/* delete node at position index */
+void linked_list_delete(node *head, int index) {
+    // do nothing if index is out of bound
+    if (index >= head->count || index < 0)
+        return;
+    // move to index
+    node *cur = head;
+    for (int i = 0; i < index; ++i) {
+        cur = cur->next;
+    }
+    // delete
+    node *old = cur->next;
+    cur->next = old->next;
+    free(old);
+    head->count--;
+}
+
+/* remove the first occurrences node of val */
+void linked_list_remove(node *head, int val) {
+    // find
+    node *cur = head;
+    while (cur->next != NULL && cur->next->value != val) {
+        cur = cur->next;
+    }
+    // return if not found
+    if (cur->next == NULL)
+        return;
+    // delete found node
+    node *old = cur->next;
+    cur->next = old->next;
+    free(old);
+    head->count--;
+}
+
+/* remove all occurrence of val */
+void linked_list_remove_all(node *head, int val) {
+    node *cur = head;
+    while (cur->next != NULL) {
+        if (cur->next->value == val) {
+            node *old = cur->next;
+            cur->next = old->next;
+            free(old);
+            head->count--;
+        } else {
+            cur = cur->next;
+        }
+    }
+}
+
+/* get value at position index */
+int linked_list_get(node *head, int index) {
+    // do nothing if out of bound
+    if (index >= head->count || index < 0)
+        return INT_MIN;
+    // search
+    node *cur = head->next;
+    for (int i = 0; i < index; ++i) {
+        cur = cur->next;
+    }
+    return cur->value;
+}
+
+/* search the first index of val */
+int linked_list_search(node *head, int val) {
+    node *cur = head;
+    int index;
+    for (index = 0; cur->next != NULL && cur->next->value != val; ++index) {
+        cur = cur->next;
+    }
+    if (cur->next == NULL)  // not found
+        return -1;
+    return index;
+}
+
+/* search all indexes of val */
+node *linked_list_search_all(node *head, int val) {
+    node *indexes = linked_list_init();
+    node *tail = indexes;
+    int idx = 0;
+    for (node *cur = head->next; cur != NULL; cur = cur->next, ++idx) {
+        if (cur->value == val) {
+            // append index at the tail
+            tail->next = (node*)malloc(sizeof(node));
+            tail = tail->next;
+            tail->value = idx;
+            indexes->count++;
+        }
+    }
+    tail->next = NULL;
+    return indexes;
+}
